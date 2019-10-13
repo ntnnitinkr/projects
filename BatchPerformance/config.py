3570ced8803437dbfbf8ea01e25a59dfd0dacdf1
ntnnitinkr <31663103+ntnnitinkr@users.noreply.github.com>
2019-10-13 11:06:56 +0530

    Add files via upload

---
+++ BatchPerformance/config.py
@@ -0,0 +1,50 @@
+#Provide configuration details for Database connection in respective fields.
+#   Example-
+#    'host': 'localhost',
+#    'dbname': 'company',
+#    'user': 'user',
+#    'password': 'password',
+#    'port': 3306
+
+# Please set the default DATABASE to retrieve batch codes for inital page (summary page)
+DEFAULT_DB = 'DBNAME1'
+
+DATABASE_4 = {
+    'host': 'XXXX.XXXX.XXXX.XXXX',
+    'dbname': 'DBNAME1',
+    'user': 'XXXX',
+    'password': 'XXXX',
+    'port': 1111
+}
+
+DATABASE_2 = {
+    'host': 'XXXX.XXXX.XXXX.XXXX',
+    'dbname': 'DBNAME2',
+    'user': 'XXXX',
+    'password': 'XXXX',
+    'port': 1111
+}
+
+DATABASE_3 = {
+    'host': 'XXXX.XXXX.XXXX.XXXX',
+    'dbname': 'DBNAME3',
+    'user': 'XXXX',
+    'password': 'XXXX',
+    'port': 1111
+}
+
+DATABASE_4 = {
+    'host': 'XXXX.XXXX.XXXX.XXXX',
+    'dbname': 'DBNAME4',
+    'user': 'XXXX',
+    'password': 'XXXX',
+    'port': 1111
+}
+
+DATABASE_5 = {
+    'host': 'XXXX.XXXX.XXXX.XXXX',
+    'dbname': 'DBNAME5',
+    'user': 'XXXX',
+    'password': 'XXXX',
+    'port': 1111
+}
